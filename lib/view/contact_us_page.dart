94e82e774d56bfd351055ca5ceafa570712c561a
MrBoOodY <abdallamohamed197@gmail.com>
2022-09-13 06:21:56 +0200

    solved overflow in contact us page

--- lib/view/contact_us_page.dart
+++ lib/view/contact_us_page.dart
@@ -36,58 +36,54 @@ class _ContactUsPageState extends State<ContactUsPage> {
                 height: MediaQuery.of(context).size.width * 0.25,
                 fit: BoxFit.cover,
               ),
-              IntrinsicHeight(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: customPadding < 0 ? 0 : customPadding),
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(color: Colors.white),
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.1),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              const Text(
-                                'CONTACT US',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w900,
-                                  fontSize: 34,
-                                  height: 4,
-                                ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: customPadding < 0 ? 0 : customPadding),
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(color: Colors.white),
+                  child: SizedBox(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.1),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            const Text(
+                              'CONTACT US',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                fontSize: 34,
+                                height: 4,
                               ),
-                              const Text(
-                                'You Can Contact Us by downloading the app from google play store or app store then sign in to the app and finally you can contact us instantly.',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 20,
-                                  height: 1.5,
-                                ),
+                            ),
+                            const Text(
+                              'You Can Contact Us by downloading the app from google play store or app store then sign in to the app and finally you can contact us instantly.',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
+                                height: 1.5,
                               ),
-                              MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushReplacementNamed(
-                                        context, Routes.download);
-                                  },
-                                  child: const Text(
-                                    'DOWNLOAD',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w900,
-                                      color: Colors.blue,
-                                      fontSize: 20,
-                                      height: 4,
-                                    ),
+                            ),
+                            MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushReplacementNamed(
+                                      context, Routes.download);
+                                },
+                                child: const Text(
+                                  'DOWNLOAD',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                    height: 4,
                                   ),
                                 ),
-                              )
-                            ]),
-                      ),
+                              ),
+                            )
+                          ]),
                     ),
                   ),
                 ),
